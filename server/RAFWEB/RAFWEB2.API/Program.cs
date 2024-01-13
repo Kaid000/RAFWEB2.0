54c435ac5d39289c3735cc550870474b7c552bb3
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-01-13 16:38:50 +0300

    Added DbContext

--- server/RAFWEB/RAFWEB2.API/Program.cs
+++ server/RAFWEB/RAFWEB2.API/Program.cs
@@ -1,8 +1,11 @@
+using RAFWEB2.Core.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
