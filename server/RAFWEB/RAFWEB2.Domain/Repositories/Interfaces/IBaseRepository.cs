3f4020e6d250d77b6e5874485f836a67af83d5e6
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-01-15 03:37:01 +0300

    Added BaseRepo

---
+++ server/RAFWEB/RAFWEB2.Domain/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace RAFWEB2.Domain.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+
+        Task<IEnumerable<T>> CreateRangeAsync(IEnumerable<T> entities);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task UpdateRangeAsync(IEnumerable<T> entities);
+
+        Task RemoveAsync(T entity);
+
+        Task<int> RemoveRangeAsync(IEnumerable<T> entities);
+
+        Task<T> GetByAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+
+        IQueryable<T> GetAllQueryable();
+    }
+}
