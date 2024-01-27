185fe00d5828454b0cfce802d6de250c1ee1ab8a
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-01-27 10:35:44 +0300

    Added DI and DTO's

---
+++ server/RAFWEB/RAFWEB2.Domain/DTO/StudentOrganizationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RAFWEB2.Domain.DTO
+{
+    public class StudentOrganizationDTO
+    {
+        public string Name { get; set; }
+        public string Leader { get; set; }
+        public string? About { get; set; }
+    }
+}
