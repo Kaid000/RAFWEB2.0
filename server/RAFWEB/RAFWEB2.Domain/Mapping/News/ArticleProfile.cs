fabaf8d14e4d30f2f386d72dda8841ec6929f1b1
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-01-27 11:07:33 +0300

    Added AutoMapper

---
+++ server/RAFWEB/RAFWEB2.Domain/Mapping/News/ArticleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RAFWEB2.Data.Models;
+using RAFWEB2.Domain.DTO;
+
+namespace RAFWEB2.Domain.Mapping.News
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDTO, Article>();
+
+            CreateMap<Article, ArticleDTO>();
+        }
+    }
+}
