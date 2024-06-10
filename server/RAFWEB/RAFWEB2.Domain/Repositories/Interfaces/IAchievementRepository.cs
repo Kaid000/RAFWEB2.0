325118579bcffc566158bc188fc61f61c34616e8
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-06-11 02:53:13 +0300

    Fixes bugs

--- server/RAFWEB/RAFWEB2.Domain/Repositories/Interfaces/IAchievementRepository.cs
+++ server/RAFWEB/RAFWEB2.Domain/Repositories/Interfaces/IAchievementRepository.cs
@@ -5,6 +5,6 @@ namespace RAFWEB2.Domain.Repositories.Interfaces
 {
     public interface IAchievementRepository : IBaseRepository<Achivement>
     {
-        Task<Achivement> CreateAsync(AchivementDTO achivement);
+        
     }
 }
