2a7c6daabace98c0b6d12d7742fd4975d49bb199
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-06-12 16:06:45 +0300

    Added FluentValidation to Articles

---
+++ server/RAFWEB/RAFWEB2.Domain/Domain/News/Commands/AddArticle/AddArticleValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using RAFWEB2.Domain.Domain.News.Command.AddArticle;
+using RAFWEB2.Domain.Repositories.Interfaces;
+
+namespace RAFWEB2.Domain.Domain.News.Commands.AddArticle
+{
+    public sealed class AddArticleValidator : AbstractValidator<AddArticleCommand>
+    {
+        public AddArticleValidator(IArticleRepository repo)
+        {
+            RuleFor(c => c.Article.Title).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Length(5, 50);
+
+            RuleFor(c => c.Article.HolidayId).Cascade(CascadeMode.StopOnFirstFailure).NotNull();
+        }
+    }
+}
