f91f68b600c393343b2f5d0b8938e7f53da463ec
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-06-12 16:15:53 +0300

    Added Validator to Organizations

---
+++ server/RAFWEB/RAFWEB2.Domain/Domain/Organization/Commands/AddOrganization/AddOrganizationValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using RAFWEB2.Domain.Repositories.Interfaces;
+
+namespace RAFWEB2.Domain.Domain.Organization.Commands.AddOrganization
+{
+    public sealed class AddOrganizationValidator : AbstractValidator<AddOrganizationCommand>
+    {
+        public AddOrganizationValidator(IOrganizationRepository repo)
+        {
+            RuleFor(c => c.StudentOrganization.Name).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Length(5, 50);
+
+            RuleFor(c => c.StudentOrganization.Leader).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Length(5, 50);
+        }
+    }
+}
