1044c4212fc4643049020bab239679dcdd57515d
Ka1d000 <Eugenevoiteshik@gmail.com>
2024-06-12 16:18:07 +0300

    Added Validator to MainPage

---
+++ server/RAFWEB/RAFWEB2.Domain/Domain/MainPage/Commands/AddPage/AddPageValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using RAFWEB2.Domain.Repositories.Interfaces;
+
+namespace RAFWEB2.Domain.Domain.MainPage.Commands.AddPage
+{
+    public sealed class AddPageValidator : AbstractValidator<AddPageCommand>
+    {
+        public AddPageValidator(IMainPageRepository repo)
+        {
+            RuleFor(c => c.MainPageContent.Title).Cascade(CascadeMode.StopOnFirstFailure).NotNull().Length(5, 50);
+        }
+    }
+}
